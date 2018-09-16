b8692078450304683b034ade6199faf75004faf0
mfazari <massimofazari@bluewin.ch>
2018-09-16 16:43:05 +0200

    Chapter 4 concluded

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-
+var fortune = require('./lib/fortune.js');
 var app = express();
 
 // set up handlebars view engine
@@ -12,6 +12,10 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+/*
+
+//Unnecessary code
+
 var fortuneCookies = [
 	"Conquer your fears or they will conquer you.",
 	"Rivers need springs.",
@@ -19,14 +23,14 @@ var fortuneCookies = [
 	"You will have a pleasant surprise.",
 	"Whenever possible, keep it simple.",
 ];
+*/
+
 
 app.get('/', function(req, res) {
 	res.render('home');
 });
 app.get('/about', function(req,res){
-	var randomFortune = 
-		fortuneCookies[Math.floor(Math.random() * fortuneCookies.length)];
-	res.render('about', { fortune: randomFortune });
+	res.render('about', { fortune: fortune.getFortune() } );
 });
 
 // 404 catch-all handler (middleware)

995379c98c7328bbdd2a8ba2aa755702deae2876
mfazari <massimofazari@bluewin.ch>
2018-09-16 14:50:14 +0200

    working meadow app

--- app.js
+++ app.js
@@ -1,43 +1,48 @@
 var express = require('express');
-var app = express();
-    // set up handlebars view engine
-var handlebars = require('express3-handlebars') .create({ defaultLayout:'main' });
-    app.engine('handlebars', handlebars.engine);
-    app.set('view engine', 'handlebars');
 
-    
+var app = express();
+
+// set up handlebars view engine
+var handlebars = require('express-handlebars')
+	.create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){ res.type('text/plain');
-            res.send('Meadowlark Travel');
+var fortuneCookies = [
+	"Conquer your fears or they will conquer you.",
+	"Rivers need springs.",
+	"Do not fear what you don't know.",
+	"You will have a pleasant surprise.",
+	"Whenever possible, keep it simple.",
+];
+
+app.get('/', function(req, res) {
+	res.render('home');
 });
-app.get('/about', function(req, res){
-            res.type('text/plain');
-            res.send('About Meadowlark Travel');
-});
-    // custom 404 page
-app.use(function(req, res, next){ res.type('text/plain');
-            res.status(404);
-            res.send('404 - Not Found');
+app.get('/about', function(req,res){
+	var randomFortune = 
+		fortuneCookies[Math.floor(Math.random() * fortuneCookies.length)];
+	res.render('about', { fortune: randomFortune });
 });
 
-
-
-    // custom 404 page
-app.use(function(req, res){ res.type('text/plain');
-            res.status(404);
-            res.send('404 - Not Found');
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
+	res.status(404);
+	res.render('404');
 });
-    // custom 500 page
-app.use(function(err, req, res, next){ console.error(err.stack);
-            res.type('text/plain');
-            res.status(500);
-            res.send('500 - Server Error');
+
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
 });
+
 app.listen(app.get('port'), function(){
-console.log( 'Express started on http://localhost:' +
-        app.get('port') + '; press Ctrl-C to terminate.' );
-    });
-
-
+  console.log( 'Express started on http://localhost:' + 
+    app.get('port') + '; press Ctrl-C to terminate.' );
+});

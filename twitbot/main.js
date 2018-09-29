88d667d251f2f16362cd4c821aecdce9ba66fadf
Massimo Fazari <massimofazari@massimos-imac.home>
2018-09-29 15:36:47 +0200

    Improved Twitbot

--- twitbot/main.js
+++ twitbot/main.js
@@ -1,17 +1,48 @@
-var Twit = require('twit')
-var T = new Twit({
-    consumer_key:         ' ... ',
-    consumer_secret:      ' ... ',
-    access_token:         ' ... ',
-    access_token_secret:  ' ... ',
-})
-var users = ["10228272", "155659213", "783214"];
-var stream = T.stream('statuses/filter', {follow: users});
-stream.on('tweet', function (tweet) {
-    if (users.indexOf(tweet.user.id_str) > -1) {
-        console.log(tweet.user.name + ": " + tweet.text);
-        T.post('statuses/retweet/:id', { id: tweet.id_str }, function (err, data, response) {
-            console.log(data)
-        })
+const twit = require('twit');
+const config = {
+    consumer_key:         'N7KMv4kAyyUGx5kqzfLqAyJ5W',
+    consumer_secret:      'Bt99VhJrInU5KDuQussAw8738Ztl7Ds0wfsR28q5MnsBZKcESV',
+    access_token:         '1045652205448179713-e9aZNvNCWBdSh3ZTmRiJ7He9Hm3LH9',
+    access_token_secret:  'Mm5ytWr1srVRNkrRL5fu1tzcuiuWMyok6rqqiQwxP7emd',
+}
+
+const Twitter = new twit(config);
+
+let retweet = function() {
+    let params = {
+        q: '#developer, #jobs',
+        result_type: 'mixed',
+        lang: 'en'
     }
-})
+    Twitter.get('search/tweets', params, function(err, data) {
+        console.log(data);
+        // if there is no error
+        if (!err) {
+           // loop through the first 4 returned tweets
+          for (let i = 0; i < 4; i++) {
+            // iterate through those first four defining a rtId that is equal to the value of each of those tweets' ids
+          let rtId = data.statuses[i].id_str;
+            // the post action
+          Twitter.post('statuses/retweet/:id', {
+            // setting the id equal to the rtId variable
+            id: rtId
+            // log response and log error
+          }, function(err, response) {
+            if (response) {
+              console.log('Successfully retweeted');
+            }
+            if (err) {
+              console.log(err);
+            }
+          });
+        }
+      }
+        else {
+            // catch all log if the search could not be executed
+          console.log('Could not search tweets.');
+        }
+    });
+}
+retweet();
+
+setInterval(retweet, 600000);

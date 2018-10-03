636d3d70b9d8ea9d6f4168ad86b4d7ead795193e
mfazari <massimofazari@bluewin.ch>
2018-10-03 20:55:51 +0200

    new

--- Timestamp/main.js
+++ Timestamp/main.js
@@ -24,53 +24,23 @@ app.get('/dateValues/:dateVal', function(req, res, next){
         day: 'numeric'
     };
 
-    if(isNaN(dateVal)){
+    if(isNaN(dateVal)){ //isnan checks whether number is an illegal number (not a number)
         var naturalDate = new Date(dateVal);
-        naturalDate = naturalDate.toLocaleDateString("en-us", dateFormattingOptions);
+        naturalDate = naturalDate.toLocaleDateString("en-us", dateFormattingOptions); // December 15, 2015
         var unixDate = new Date(dateVal).getTime()/1000;
     }
     else{
         var unixDate = dateVal;
         var naturalDate = new Date(dateVal * 1000);
+
         naturalDate = naturalDate.toLocaleDateString("en-us", dateFormattingOptions);
+
     }
     res.json({unix: dateVal, natural: naturalDate});
 
 });
 
-
-
 app.listen(3000, function(){
 
     console.log("Working");
 });
-
-
-
-
-
-
-/*
-
-var app = module.exports = express();
-app.use(bodyparser.json());
-app.use(cors());
-
-//Main
-app.get('/dateValues/:dateVal', function(req, res, next){
-
-    var dateVal = req.params.dateVal;
-    res.json({unix: dateVal});
-
-});
-
-
-
-
-app.listen(3000, function(){
-
-    console.log("Working");
-});
-
-
-*/
